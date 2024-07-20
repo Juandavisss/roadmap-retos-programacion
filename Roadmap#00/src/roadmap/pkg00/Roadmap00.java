debfdf7296d90b750a95d1f6365a79076760e563
kaoss <kaoss@192.168.1.12>
2024-07-21 00:27:12 +0200

    Primer punto del Roadmap

---
+++ Roadmap#00/src/roadmap/pkg00/Roadmap00.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package roadmap.pkg00;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kaoss
+ */
+public class Roadmap00 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        //https://www.java.com/es/
+        //En una linea
+        /*
+        En varias
+        lineas
+        */
+        int num=5;
+        
+        final String Java="java";
+        
+        byte uno=1;
+        short poco=2;
+        int numero = 10;
+        long largo=20000;
+        float decima = 22.2f;
+        double mayor = 34554.2;
+        char inicial = 'J';
+        boolean siono = false;
+        
+        System.out.println("¡Hola, "+Java);
+    }
+    
+}
